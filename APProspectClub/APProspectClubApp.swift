88562b97cd4869639043daf829a40f5ae08861f9
Gia Bao Ta <giabaota2020@gmail.com>
2023-12-14 10:34:16 -0600

    Initial Commit

---
+++ APProspectClub/APProspectClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  APProspectClubApp.swift
+//  APProspectClub
+//
+//  Created by Gia Bao Ta on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct APProspectClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
