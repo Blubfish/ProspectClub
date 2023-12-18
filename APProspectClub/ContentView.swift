389ee0a6e605d86706c560aabe2aa972b37bd477
Andres Lopez <alopez4578@stu.d214.org>
2023-12-18 10:10:05 -0600

    m

--- APProspectClub/ContentView.swift
+++ APProspectClub/ContentView.swift
@@ -10,10 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world")
+            
+            LinearGradient(colors: [Color.cyan.opacity(0.7),Color.purple.opacity(0.3)], startPoint: .topLeading, endPoint: .bottomTrailing)
+            
+            Circle()
+                .frame(width:300)
+                .foregroundColor(Color.blue.opacity(0.3))
+                .blur(radius: 10)
+                .offset(x:-100, y: -100)
+            
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .frame(width: 500, height:500)
+                .foregroundColor(LinearGradient(colors: [Color.pruple.opacity(0.5)], startPoint: .top, endPoint: .leading))
+                
+            
+            Text("PHS Clubs")
+                .foregroundColor(.black)
+                .font(.system(size: 70, weight: .heavy, design: .default))
+                .offset(y:-150)
+            
+            Image("PHS")
+                .resizable()
+                .frame(width: 200, height: 200)
+                .offset(y:-180)
+                .offset(x:-20)
         }
         .padding()
     }
