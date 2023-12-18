83656c13b52c04173803b949c6809660dc6820f4
Andres Lopez <alopez4578@stu.d214.org>
2023-12-18 11:13:51 -0600

    n

--- APProspectClub/ContentView.swift
+++ APProspectClub/ContentView.swift
@@ -9,35 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var database = Database()
+    @State private var searchTerm = ""
     
     var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .cyan]),
-                           startPoint: .leading, endPoint: .trailing)
-                .edgesIgnoringSafeArea(.all)
+        NavigationStack {
             
-            VStack {
-                Text("PHS Clubs")
-                    .foregroundColor(.black)
-                    .font(.system(size: 70, weight: .heavy, design: .default))
-                    .offset(y: -150)
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [.blue, .cyan]),
+                               startPoint: .leading, endPoint: .trailing)
+                .edgesIgnoringSafeArea(.all)
                 
-                Image("PHS")
-                    .resizable()
-                    .frame(width: 200, height: 200)
-                    .offset(y: -180)
-                    .offset(x: -20)
+                VStack {
+                    Text("PHS Clubs")
+                        .foregroundColor(.white)
+                        .font(.system(size: 70, weight: .heavy, design: .default))
+                        .offset(y: -150)
+                    
+                    Rectangle()
+                        .frame(width: 100, height: 100)
+                        
+                    
+                    Image("PHS")
+                        .resizable()
+                        .frame(width: 150, height: 150)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 2)) // Optional: add a border
+                        .shadow(radius: 4)
+                        .offset(y: -180)
+                        .offset(x: -20)
+                }
+                .padding()
+                .onAppear() {
+                    self.database.getData()
+                }
             }
-            .padding()
-            .onAppear() {
-                self.database.getData()
-            }
-        }
+        } .searchable(text: $searchTerm, prompt: "Search Clubs")
+            .foregroundColor(.black)
+            
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
